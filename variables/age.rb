036eb6f43b4d18747deda3d60404cef085270a20
Khalid Hussain <Khalid@KhalidHussain.com>
2020-03-03 12:56:31 +0000

    Added Chapter 2 exercises.

---
+++ variables/age.rb
@@ -0,0 +1,6 @@
+print "How old are you? "
+age = gets.chomp.to_i
+puts "In ten years you'll be #{age + 10}"
+puts "In twenty years you'll be #{age + 20}"
+puts "In thirty years you'll be #{age + 30}"
+puts "In forty years you'll be #{age + 40}"
