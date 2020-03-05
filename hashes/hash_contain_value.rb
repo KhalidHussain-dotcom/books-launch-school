23f4408423b11ccfd72a3c7b5664cd1660588a7c
Khalid Hussain <Khalid@KhalidHussain.com>
2020-03-05 09:54:51 +0000

    Added Chapter 7 Exercises.

---
+++ hashes/hash_contain_value.rb
@@ -0,0 +1,3 @@
+hash = { name: "Khalid", age: 39, weight: "170 lbs", hair: "brown" }
+
+puts hash.value?(39)
---
+++ hashes/merge_class.rb
@@ -0,0 +1,9 @@
+hash1 = { name: "Khalid" }
+hash2 = { age: 39 }
+
+puts hash1.merge(hash2)
+puts hash1.merge!(hash2)
+
+
+
+
